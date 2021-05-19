326e514f853a56169d88055319744456e3ce4fad
Shubhadeep Mukherjee <smukherjee@groupon.com>
2021-05-19 15:30:04 +0530

    minstepsbyknight

--- Algo/problems2/min_steps_by_knight_2.py
+++ Algo/problems2/min_steps_by_knight_2.py
@@ -17,70 +17,45 @@ def is_valid(input_point: Pair, rows, cols):
         return True
 
 
-def neighbour_check(u, v, q, rows, cols, flag, visited_matrix, ans, destination):
-    # print(f"u- {u} v-{v}")
-    # print(f"checking against point {u}, {v} visited_matrix = {visited_matrix[u][v]} flag = {flag}")
-    # print(visited_matrix)
-    dest_found_flag = False
-    if is_valid(Pair(u, v), rows, cols) and visited_matrix[u][v]==0:
-        print(f"checking against point {u}, {v} visited_matrix = {visited_matrix[u][v]} flag = {flag}")
-        if flag is False:
-            flag = True
-            ans += 1
+def neighbour_check(u, v, q, rows, cols, input_2d_matrix, destination):
+    print(f"u- {u} v-{v}")
+    if is_valid(Pair(u, v), rows, cols) and input_2d_matrix[u][v]==0:
+        print(f"checking against point {u}, {v} input_2d_matrix = {input_2d_matrix[u][v]} ")
         # mark neighbour co-ordinate visited
-        visited_matrix[u][v] = True
+        input_2d_matrix[u][v] = 1
         q.append(Pair(u, v))
         # checking if destination is reached
         if u == destination[0] and v == destination[1]:
             print(f"MATCH FOUND")
-            dest_found_flag = True
-            return flag, ans, dest_found_flag
-        return flag, ans, dest_found_flag
-    else:
-        # print(f"checks - {is_valid(Pair(u, v), rows, cols)}")
-        return False, ans, dest_found_flag
+            return True
+        print(input_2d_matrix)
+    return False
 
 
-def bfs_helper_i_j(i, j, two_d_matrix, q, rows, cols, destination):
-
-    # updating matrix to mark as visited
-    two_d_matrix[i][j] = 1
-
-    # look for possible paths from this item point
-    # i,j
-    # check for (i-1,j-2), (i-1,j+2)
-    flag, ans, destination_flag = neighbour_check(i-1, j-2, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
-    flag, ans, destination_flag = neighbour_check(i-1, j+2, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
+def bfs_from_i_j(input_2d_matrix, i, j, destination, q, rows, cols):
+    if neighbour_check(i-1, j-2, q, rows, cols, input_2d_matrix, destination):
+        return True
+    if neighbour_check(i-1, j+2, q, rows, cols, input_2d_matrix, destination):
+        return True
 
     # check for (i+1,j-2), (i+1,j+2)
-    flag, ans, destination_flag = neighbour_check(i+1, j-2, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
-    flag, ans, destination_flag = neighbour_check(i+1, j+2, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
+    if neighbour_check(i+1, j-2, q, rows, cols, input_2d_matrix, destination):
+        return True
+    if neighbour_check(i+1, j+2, q, rows, cols, input_2d_matrix, destination):
+        return True
 
     # check for (i+2,j-1), (i+2,j+1)
-    flag, ans, destination_flag = neighbour_check(i+2, j-1, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
-    flag, ans, destination_flag = neighbour_check(i+2, j+1, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
+    if neighbour_check(i+2, j-1, q, rows, cols, input_2d_matrix, destination):
+        return True
+    if neighbour_check(i+2, j+1, q, rows, cols, input_2d_matrix, destination):
+        return True
 
     # check for (i-2,j-1), (i-2,j+1)
-    flag, ans, destination_flag = neighbour_check(i-2, j-1, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
-    flag, ans, destination_flag = neighbour_check(i-2, j+1, q, rows, cols, flag, visited_matrix, ans, destination)
-    if destination_flag:
-        return ans
-
-    pass
+    if neighbour_check(i-2, j-1, q, rows, cols, input_2d_matrix, destination):
+        return True
+    if neighbour_check(i-2, j+1, q, rows, cols, input_2d_matrix, destination):
+        return True
+    return False
 
 
 def min_steps_by_knight(input_2d_matrix, start_point, destination):
@@ -88,17 +63,9 @@ def min_steps_by_knight(input_2d_matrix, start_point, destination):
     cols = max(len(line) for line in input_2d_matrix)
     rows = len(input_2d_matrix)
 
-    # visited_matrix = list()
-    # for i in range(rows):
-    #     visited_matrix.append([])
-    #     for j in range(cols):
-    #         visited_matrix[i].append(False)
-
-    # [[False] * cols]*rows
-
-    # print(f"visited_matrix - {visited_matrix}")
+    print(f"visited_matrix - {input_2d_matrix}")
     q = deque()
-    q.append(Pair(start_point[0], start_point[1]))  # start point x,y co-ordinates
+    q.append(Pair(start_point[0], start_point[1]))
     q.append(Pair(None, None))
     ans = 0
 
@@ -111,7 +78,7 @@ def min_steps_by_knight(input_2d_matrix, start_point, destination):
         if len(q) == 0:
             break
 
-        flag = False  # this is marking 1 step for all neighbour positions that can be reached from i,j
+        ans += 1  # increase ans by 1 when all possible moves from i,j has been checked and target not found
 
         # untill None pair is reached, reaching None means we have covered all neighbours who can be reached from i,j
         while q[0].x is not None and q[0].y is not None:
@@ -119,16 +86,17 @@ def min_steps_by_knight(input_2d_matrix, start_point, destination):
             print(f"poped queue item - ({item.x} , {item.y})")
             i = item.x
             j = item.y
+            input_2d_matrix[i][j] = 1
 
-            bfs_helper_i_j(i, j, input_2d_matrix, q, rows, cols, destination)
-
+            if bfs_from_i_j(input_2d_matrix, i, j, destination, q, rows, cols):
+                return ans
             # Below is done to mark that all neighbour visited for item
             q.append(Pair(None, None))
 
             # final queue content
             for i in q:
                 print(f"({i.x},{i.y}) - > ", end="")
-            print(f"\nneighbour check completed for item - {item.x} , {item.y}")
+            print(f"\nneighbour check completed for item - {item.x} , {item.y} ans={ans}")
             print("------------")
 
     return -99
@@ -137,7 +105,7 @@ def min_steps_by_knight(input_2d_matrix, start_point, destination):
 if __name__ == "__main__":
     print("")
 
-    matrix_2d = [[0 for j in range(6)] for i in range(6)]  # this kind of operation will create copies of reference as list is an object, any update will update all columns
+    matrix_2d = [[0 for j in range(6)] for i in range(6)] # this kind of operation will create copies of reference as list is an object, any update will update all columns
     input_start_point = (1, 3)
     input_destination = (5, 0)
 
